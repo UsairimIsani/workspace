ffe6261f5466ab1b26261f8dcd9ff15a7e7efc4a
Usairim Isani <usairim.isani@outlook.com>
2021-03-22 17:00:49 +0500

    Setting Up Project for Development.

--- workspace-watcher/src/lib.rs
+++ workspace-watcher/src/lib.rs
@@ -1,12 +1,10 @@
 //! Workspace watcher is part the greater workspace system that i'll be building
-//! to backup my stuff on gitlab in Private repos so that i get 10GB of space for each folder 
+//! to backup my stuff on gitlab in Private repos so that i get 10GB of space for each folder
 //! ]since each folder will be a git repo.
-//! what else should i add 
-pub fn workspace_watcher(){
+//! what else should i add
+pub fn workspace_watcher() {
     println!("Hello World!");
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -14,7 +12,8 @@ mod tests {
     fn it_works() {
         use super::*;
         workspace_watcher();
-        assert_eq!("usairim","usairim");
+        assert_eq!("usairim", "usairim");
         assert_eq!(2 + 2, 4);
+        ////
     }
 }
